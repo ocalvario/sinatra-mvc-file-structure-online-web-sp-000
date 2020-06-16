8f521dc44286037ef638481980f625954bfb4e76
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-16 03:04:11 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,16 +1,18 @@
 class Dog
-  attr_reader :breed
-  attr_accessor :name, :age
 
-  @@dogs = []
+  attr_accessor :name, :breed, :age
+
+    @@all = []
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    @@dogs << self
+    @@all << self
   end
 
   def self.all
-    @@dogs
+      @@all
   end
+
 end
